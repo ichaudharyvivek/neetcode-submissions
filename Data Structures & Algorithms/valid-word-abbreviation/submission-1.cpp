2dda669af870a2bd07ada951369aab28a4617df8
Vivek Chaudhary <51666451+ichaudharyvivek@users.noreply.github.com>
2026-09-26 02:23:47 +0530

    Add: valid-word-abbreviation - submission-1

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    bool validWordAbbreviation(string word, string abbr) {
+        if (word == abbr) {
+            return true;
+        }
+        int m = word.size(), n = abbr.size();
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            cout<<word[i]<<", "<<abbr[j]<<endl;
+            if (abbr[j] == '0') {
+                return false;
+            }
+            if (isdigit(abbr[j])) {
+                int k = j;
+                while (isdigit(abbr[k])) {
+                    k++;
+                }
+                int num = stoi(abbr.substr(j, k - j + 1));
+                i += num;
+                j = k;
+            }
+            if (i>m || j>n || word[i] != abbr[j]) {
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+};
